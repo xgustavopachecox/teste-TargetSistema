087053fb3023f6e64df5d364686c2714cf6efd1c
Gustavo de Castro Pacheco <130229305+xgustavopachecox@users.noreply.github.com>
2024-03-11 21:17:39 -0300

    Add files via upload

---
+++ ex1/main.cpp
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cppFiles/main.cc to edit this template
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: User
+ *
+ * Created on 11 de março de 2024, 21:16
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    int indice = 13, soma =0, k=0;
+    
+    while(k<indice){
+        k += 1;
+        soma +=k;
+    }
+    cout<<soma;
+    return 0;
+}
+
